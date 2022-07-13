dffe7fa10de4de387b5c1ba886a8a372385e4b57
Kirill <krl.gushchinn@mail.ru>
2022-07-13 08:08:12 -0700

    Готовое задание № 4

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿//Программа определяющая максимальное число 
+using System;
+
+public class Zadanie4
+{
+    public static void Main(string[] args)
+    {
+        int a,b,c,max;
+        Console.Write ("Введите число a=");
+        a=int.Parse(Console.ReadLine());
+        
+        Console.Write ("Введите число b=");
+        b=int.Parse(Console.ReadLine());
+        
+        Console.Write ("Введите число c=");
+        c=int.Parse(Console.ReadLine());
+        
+        if (a>b) {
+            max=a;
+        }
+        else {
+            max=b;
+        }
+        if (max<c) {
+            max=c;
+        }
+        
+         Console.WriteLine ($"max={max}");
+    }
+}
